2fa2b169ca6aa94fb547ca70947f4d05b4c1752e
Nodoka Masamune <nmasamune9174@sdsu.edu>
2025-11-03 13:26:50 -0800

    - edits to likelihood

--- src/likelihood.py
+++ src/likelihood.py
@@ -3,7 +3,7 @@
 #imports
 import numpy as np
 from data_reader import data_points_reader, covariance_reader
-from cosmology import luminosity_distance, distance_modulus, intd, eta
+from cosmology import distance_modulus
 
 def data():
     dat = data_points_reader('../data/jla_mub.txt')
@@ -15,54 +15,48 @@ def data():
     return z_data, mu_data, Cinv
 
 
-class likelihood:
-    def __init__(self, z_data, mu_data, Cinv, model_func):
-        self.z_data = z_data
-        self.mu_data = mu_data
-        self.Cinv = Cinv
+def log_likelihood(theta, z_data, mu_data, Cinv):
+    # log likelihood for gaussian errors with covariance C eqn:
+    #ln L(theta) = - 1/2 n-sum (1, j = 1) r_i [C-1]_ij r_j
+    #r_i = mu_i^obs - mu_i^theory(z_i;theta)
+    Omega_m, h = theta
 
-    def log_prior(self, theta):
-        # prior probabilities
-        Omega_m, h = theta
-        if 0.0 < Omega_m < 1.0 and 0.4 < h < 1.0:
-            return 0.0 # log (1.0)
-        else:
-            return -np.inf
-        
-    def log_likelihood(self, theta):
-        # log likelihood for gaussian errors with covariance C eqn:
-        #ln L(theta) = - 1/2 n-sum (1, j = 1) r_i [C-1]_ij r_j
-        #r_i = mu_i^obs - mu_i^theory(z_i;theta)
+    # theoretical mu
+    mu_theory = np.array([[distance_modulus(z, Omega_m, h=h, Pen=True)] for z in z_data])
+    
+    # residuals
+    r = mu_data - mu_theory
 
-        Omega_m, h = theta
-        # theoretical mu
-        mu_theory = np.array([self.model_func(z, Omega_m, h=h, Pen=True) for z in self.z_data])
-        # residuals
-        r = self.mu_data - mu_theory
-        
-        loglikelihood = - 1/2 * np.dot(r, np.dot(self.Cinv, r))
+    loglikelihood = - 1/2 * np.dot(r, np.dot(Cinv, r))
 
-        return loglikelihood
-
-    def log_posterior(self, theta):
-        # way to sample the posterior
-
-        lp = self.log_prior(theta)
-
-        if not np.isfinite(lp):
-            return -np.inf
-        llh = self.log_likelihood(theta)
-
-        return lp + llh  
+    return loglikelihood
 
 
+def log_prior(theta):
+    # prior probabilities
+    Omega_m, h = theta
+    if 0.0 < Omega_m < 1.0 and 0.4 < h < 1.0:
+        return 0.0 # log (1.0)
+    else:
+        return -np.inf
+
+def log_posterior(theta, z_data, mu_data, Cinv):
+    # way to sample the posterior
+
+    lp = log_prior(theta)
+
+    if not np.isfinite(lp):
+        return -np.inf
+    llh = log_likelihood(theta, z_data, mu_data, Cinv)
+
+    return lp + llh
 
 '''
-make into class?
+make into class? - no
 
-Invert C once at the start using np.linalg.inv(C), then reuse Cinv
+Invert C once at the start using np.linalg.inv(C), then reuse Cinv - yes
 
-Use flat (uniform) priors with physical bounds (think about reasonable ranges from the Scientific Background)
+Use flat (uniform) priors with physical bounds (think about reasonable ranges from the Scientific Background) omega=0.3, h= [0.4, 1.0]
 
 Return -np.inf for parameters outside prior bounds
 '''
